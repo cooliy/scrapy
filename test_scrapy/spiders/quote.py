570cddb193a57bd75a6544157a6c0362971bd4c7
cooliy <8555973@qq.com>
2018-10-09 22:08:10 +0800

    删除去重代码，scrapy可以自动去重

---
+++ test_scrapy/spiders/quote.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import QuoteItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quote'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.css('.quote')
+        for quote in quotes:
+            item = QuoteItem()
+            item['text'] = quote.css('.text::text').extract_first()
+            item['author'] = quote.css('.author::text').extract_first()
+            item['tags'] = quote.css('.tags .tag::text').extract()
+            yield item
+        next = response.css('.pager .netx a::attr(href)').extract_first()
+        url = response.urljoin(next)
